2e3944f9a1352aecc660ec12b88a763fcc0ac8b2
will2149 <will42224@gmail.com>
2019-10-15 12:19:11 +0100

    dice game for 2 players 2

--- demo/dice_game.py
+++ demo/dice_game.py
@@ -14,40 +14,59 @@ def main(input3=None):
     if input1 == "player1":
         input2 = input("please type your password")
     else:
-        input1 = input("please log in player one type your username")
+        if input1 == "player1":
+            input2 = input("please type your password")
 
     if input2 == "password1":
         print("welcome to the game")
 
     else:
-        input2 = input("please type your password")
+        if input2 == "password1":
+            print("welcome to the game")
+
+    input3 = input("please type your username player2")
 
     if input3 == "player2":
-        print("welcome to the game")
+        input4 = input("please type your password player2")
 
     else:
-        input3 = input("please type your username player2")
-
-        input4 = input("player2 please type your password")
+        if input3 == "player2":
+            input4 = input("please type your password player2")
 
     if input4 == "password2":
         print("welcome to the game")
 
     else:
-        input4 = input("please type your password playerd")
+        if input4 == "password2":
+            print("welcome to the")
 
     game()
 
-def game():
+
+def game(total=None):
+    global input6, input7
     print("this is now the actual game the rules are simple you get two dice roles each and there are 5 rounds of the "
-           "game and the persdn with the highest score at the ends wins")
+          "game and the person with the highest score at the ends wins")
 
     input1 = input("please type roll to roll the dice")
 
     if input1 == "roll":
-        print(random.randint(0, 6))
+        input6 = input(random.randint(0, 6))
     else:
-        input1 = input("please type roll to begin")
+        if input1 == "roll":
+            input6 = input(random.randint(0, 6))
+
+    input1 = input("please type roll to begin your second role player 1")
+
+    if input1 == "roll":
+        input7 = input(random.randint(0, 6))
+    else:
+        if input1 == "roll":
+            input7 = input(random.randint(0, 6))
+
+    total = input6 + input7
+    print("your total is", total)
+
 
 if __name__ == '__main__':
     main()
