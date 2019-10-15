5049b6575ff1124df056c90dbe95136ff2fe7d99
will2149 <will42224@gmail.com>
2019-10-15 13:08:53 +0100

    dice game for 2 players version 3 completed

---
+++ demo/test_2.py
@@ -0,0 +1,28 @@
+import random
+
+
+def adding():
+    pass
+
+    def add(self):
+        input1 = input('please type roll to roll the dice')
+
+        if input1 == "roll":
+            input2 = input(random.randint(0, 6))
+
+        input3 = input('please type roll to begin your second role')
+
+        if input3 == "roll":
+            input4 = input(random.randint(0, 6))
+
+        total = input2 + input4
+
+        print("your total is", total)
+
+
+def adding() -> object:
+    pass
+
+
+if __name__ == '__main__':
+    adding()
